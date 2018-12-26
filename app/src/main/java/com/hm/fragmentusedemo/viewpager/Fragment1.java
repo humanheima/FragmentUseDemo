6e94a824116e3342c6445ea1e21a35064879aa83
humanheima <humanheima@gmail.com>
2018-12-26 19:42:48 +0800

    test delete fragment. dmw

--- app/src/main/java/com/hm/fragmentusedemo/viewpager/Fragment1.java
+++ app/src/main/java/com/hm/fragmentusedemo/viewpager/Fragment1.java
@@ -1,8 +1,7 @@
 package com.hm.fragmentusedemo.viewpager;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.util.Log;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,32 +11,27 @@ import android.widget.Toast;
 import com.hm.fragmentusedemo.R;
 import com.hm.fragmentusedemo.utils.Images;
 
-public class Fragment1 extends SimpleBaseFragment {
-
+public class Fragment1 extends Fragment {
 
     private final String image_path = Images.imageUrls[0];
-    private View rootView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Log.e(TAG, "onCreateView: ");
-        if (rootView == null) {
-            Log.e(TAG, "onCreateView: rootView==null");
-            rootView = inflater.inflate(R.layout.f1, null);
+        View view = inflater.inflate(R.layout.f1, null);
 
-            TextView textView = rootView.findViewById(R.id.f1textview);
-            textView.setOnClickListener(new View.OnClickListener() {
+        TextView textView = view.findViewById(R.id.f1textview);
+        textView.setOnClickListener(new View.OnClickListener() {
 
-                @Override
-                public void onClick(View arg0) {
-                    Toast.makeText(getActivity(), "ok", Toast.LENGTH_LONG).show();
-                }
-            });
-        }else {
-            Log.e(TAG, "onCreateView: rootView!=null");
-        }
-
-        return rootView;
+            @Override
+            public void onClick(View arg0) {
+                Toast.makeText(getActivity(), "ok", Toast.LENGTH_LONG).show();
+            }
+        });
+        return view;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
 }
